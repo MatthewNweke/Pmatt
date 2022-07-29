b93251cd2292b55a53ec583e7d9a715642f503d3
Nweke Charles <nocrah4@gmail.com>
2022-07-29 18:24:12 +0100

    modified

--- login.js
+++ login.js
@@ -3,16 +3,16 @@ let getUsername = document.querySelector("#email-username");
 let getPassword = document.querySelector(".password1");
 
 getSubmitBtn.addEventListener("click", (e) => {
-    if (getUsername.value === "" || getPassword.value === "") {
+    if (getUsername.value.trim() === "" || getPassword.value.trim() === "") {
         e.preventDefault();
     }
 });
 getSubmitBtn.addEventListener("mouseover",()=>{
-    if (getUsername.value !== "" && getPassword.value !== "") {
+    if (getUsername.value.trim() !== "" && getPassword.value.trim() !== "") {
         getSubmitBtn.style.opacity="1";
         getSubmitBtn.classList.add("changeCursor");
     }
-    if (getUsername.value === "" || getPassword.value === "") {
+    if (getUsername.value.trim() === "" || getPassword.value.trim() === "") {
         getSubmitBtn.style.opacity="0.5";
         getSubmitBtn.classList.remove("changeCursor");
     }
